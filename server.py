b8f10b6b2b1f0e073b45816c0b62cee82a09b509
Maurice Cooper <jc70240@gmail.com>
2020-04-09 04:22:55 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('./index.html')
+
+
+@app.route('/blog')
+def blog():
+    return "Hello bloggy!"
+
